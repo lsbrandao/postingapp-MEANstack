71cdab20fe872b9f3bf648bb154bfa902b3ad701
Luiz <luiz.sb@live.com>
2019-01-09 17:34:54 -0500

    Updated header with user status

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,16 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { AuthData } from './auth-data.module';
-import { StringifyOptions } from 'querystring';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root'})
 export class AuthService {
   private token: string;
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient) {}
 
   getToken() {
     return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 
 
@@ -28,6 +33,7 @@ export class AuthService {
     this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
       .subscribe(response => {
         this.token = response.token;
+        this.authStatusListener.next(true);
       });
   }
 }
